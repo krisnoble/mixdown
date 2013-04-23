b69c66d98111cf5f90add7345e7ea8e3cfb78dcd
Anthony KIlleen <mrqwest@gmail.com>
2013-04-23 21:56:00 +0100

    good progress. SC data now stored into an array; just need to show it meaningfully

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<title>Brockout ~ A Better sort tool for SoundCloud</title>
 
 	<!-- If JS is available, changes <html> class to js -->
 	<script>
@@ -24,6 +24,41 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
 </head>
 <body>
+	<h1>BrockOut</h1>
+
+	<?php
+		ini_set('display_errors', 'On');
+		error_reporting(E_ALL);
+		require_once 'Services/Soundcloud.php';
+
+		// create a client object with your app credentials
+		$client = new Services_Soundcloud('91bd52531c9b150e11efac29abdb79eb');
+
+		// find all sounds of buskers licensed under 'creative commons share alike'
+		$tracks = $client->get(
+			'tracks', array('q' => 'dubstep', 'downloadable' => 'true', 'duration' > '1800000000'));
+
+		$data = json_decode($tracks);
+
+		function writeList($data){
+	    	global $data;
+	    	global $tracks;
+	    	echo '<ul>';
+
+	    	$data = explode(',', $tracks);
+	    	foreach($data as $mix){
+	        	echo '<li>'.$mix;
+	        	if(isset($data[$mix]))
+	            writeList($data[$mix]);
+	        	echo '</li>';
+    	}
+
+    	echo '</ul>';
+	}
+writeList($data[0]);
+		
+		?>
+
 	<script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
